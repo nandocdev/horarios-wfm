a2e88d0456b9e8af31ed51113b060fe186a18009
Fernando Castillo <ferncastillo@css.gob.pa>
2026-07-06 15:47:23 -0500

    refactor(bootstrap): validar clave de Reverb antes de inicializar Echo y actualizar ruta local de sincronizacion
    [AUTO-HANDLED by Antigravity]

--- sincroniza.sh
+++ sincroniza.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SRC="/home/ferncastillo/Projects/WorkForce/horarios-wfm/"
+SRC="/home/ferncastillo/Proyectos/php/horarios-wfm/"
 DEST="ferncastillo@10.19.15.240:/var/www/wfm-scheduler/"
 
 # Validar que estamos en la rama develop

5b50d5dbda565d7271ff06ca912c6b81006f9492
Fernando Castillo <ferncastillo@css.gob.pa>
2026-07-10 13:54:45 -0500

    fix(sincroniza): actualizar dirección de destino para la sincronización

--- sincroniza.sh
+++ sincroniza.sh
@@ -3,7 +3,7 @@
 set -e
 
 SRC="/home/ferncastillo/Proyectos/php/horarios-wfm/"
-DEST="ferncastillo@10.19.15.240:/var/www/wfm-scheduler/"
+DEST="ferncastillo@10.19.14.31:/srv/opsis/"
 
 # Validar que estamos en la rama develop
 cd "$SRC"

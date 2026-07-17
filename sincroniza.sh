08ded0c207741eed3a393ee3a0a8578207c8f67d
Fernando Castillo <ferncastillo@css.gob.pa>
2026-07-17 14:08:12 -0500

    chore: update destination server address in synchronization script

--- sincroniza.sh
+++ sincroniza.sh
@@ -3,8 +3,8 @@
 set -e
 
 SRC="/home/ferncastillo/Proyectos/php/horarios-wfm/"
-#DEST="ferncastillo@10.19.15.240:/var/www/wfm-scheduler/"
-DEST="ferncastillo@10.19.14.31:/var/www/opsis/"
+DEST="ferncastillo@10.19.15.240:/var/www/wfm-scheduler/"
+# DEST="ferncastillo@10.19.14.31:/var/www/opsis/"
 
 # Validar que estamos en la rama develop
 cd "$SRC"

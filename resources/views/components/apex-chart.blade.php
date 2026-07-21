6362c6571c79754e36aef60218691a9928889309
Fernando Castillo <ferncastillo@css.gob.pa>
2026-07-21 15:43:20 -0500

    fix(charts): convertir strings de funciones JS a funciones reales en runtime
    - xaxis.labels.formatter, yaxis.labels.formatter y tooltip.custom
      se serializan como string en JSON. El componente los convierte a
      funciones via new Function() en el Alpine x-init.
    - Base64 encoding para evitar HTML escaping de JSON en el atributo.

--- resources/views/components/apex-chart.blade.php
+++ resources/views/components/apex-chart.blade.php
@@ -5,16 +5,28 @@
 ])
 
 <div wire:ignore>
-    <div id="{{ $id }}" style="height: {{ $height }}px;" x-data="{
-        chart: null,
-        init() {
-            this.chart = new ApexCharts(this.$el, {{ $options }});
-            this.chart.render();
-        },
-        destroy() {
-            if (this.chart) {
-                this.chart.destroy();
-            }
-        }
-    }" x-init="init" x-on:livewire:navigating.window="destroy"></div>
+    <div id="{{ $id }}" style="height: {{ $height }}px;"
+         x-data="{
+            chart: null,
+            baseOptions: JSON.parse(atob('{{ base64_encode($options) }}')),
+            init() {
+                let opts = JSON.parse(JSON.stringify(this.baseOptions));
+                if (opts.xaxis?.labels?.formatter && typeof opts.xaxis.labels.formatter === 'string') {
+                    opts.xaxis.labels.formatter = (new Function('return ' + opts.xaxis.labels.formatter))();
+                }
+                if (opts.yaxis?.labels?.formatter && typeof opts.yaxis.labels.formatter === 'string') {
+                    opts.yaxis.labels.formatter = (new Function('return ' + opts.yaxis.labels.formatter))();
+                }
+                if (opts.tooltip?.custom && typeof opts.tooltip.custom === 'string') {
+                    opts.tooltip.custom = (new Function('return ' + opts.tooltip.custom))();
+                }
+                this.chart = new ApexCharts(this.$el, opts);
+                this.chart.render();
+            },
+            destroy() {
+                if (this.chart) this.chart.destroy();
+            },
+         }"
+         x-init="init"
+         x-on:livewire:navigating.window="destroy"></div>
 </div>

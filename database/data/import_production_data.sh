3a526264be69ed634e34e3074af372575d35d4c8
Fernando Castillo <ferncastillo@css.gob.pa>
2026-07-28 08:27:41 -0500

    chore: configurar defaults de produccion en import_production_data.sh

--- database/data/import_production_data.sh
+++ database/data/import_production_data.sh
@@ -25,8 +25,8 @@ fi
 
 DB_HOST="${DB_HOST:-127.0.0.1}"
 DB_PORT="${DB_PORT:-5432}"
-DB_DATABASE="${DB_DATABASE:-horarios_wfm}"
-DB_USERNAME="${DB_USERNAME:-postgres}"
+DB_DATABASE="${DB_DATABASE:-opsisdb}"
+DB_USERNAME="${DB_USERNAME:-ecmadmin}"
 DB_PASSWORD="${DB_PASSWORD:-}"
 
 ECM_DIR="database/data/ecm_db"

d56aaeafade5369b0848653782d53afedee46c97
Fernando Castillo <ferncastillo@css.gob.pa>
2026-07-31 11:10:31 -0500

    style(audit): remover imports no utilizados

--- app/Modules/AuditModule/Models/AuditLog.php
+++ app/Modules/AuditModule/Models/AuditLog.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Facades\DB;
 
 class AuditLog extends Model
 {
